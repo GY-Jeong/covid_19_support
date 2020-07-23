d2709cdb0c8a0cf678fe961c4df8b36550421e42
Byeong Chan Kim <dkdlxj9797@gmail.com>
2020-07-23 21:26:26 +0900

    adapter 연결

--- app/src/main/java/com/example/covid_19_support/ResultActivity.kt
+++ app/src/main/java/com/example/covid_19_support/ResultActivity.kt
@@ -3,6 +3,9 @@ package com.example.covid_19_support
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ResultActivity : AppCompatActivity() {
 
@@ -11,6 +14,9 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_result)
+
+        val recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
 
         val i = intent
         val isValid = i.getBooleanExtra("isValid", true)
@@ -21,18 +27,27 @@ class ResultActivity : AppCompatActivity() {
 
         if(i.hasExtra("keywordList")) {
             keywordSearchOption = true
-            val keywordOptionList = i.getStringArrayListExtra("keywordList")
+            val keywordOptionList = i.getStringArrayListExtra("keywordList") as ArrayList<String>
             for(i in keywordOptionList) {
                 Log.i("keywordOption", i)
             }
         }
 
-        val locationOptionList = i.getStringArrayListExtra("locationList")
+        val locationOptionList = i.getStringArrayListExtra("locationList") as ArrayList<String>
         for(i in locationOptionList) {
             Log.i("locationOption", i)
         }
 
         Log.i("isValid", isValid.toString())
+    }
+    fun searchFB(keyword:ArrayList<String> , location:ArrayList<String>):Int {
+        // TODO: 2020-07-23 파베에서 읽는 코드를 넣어야함 인자랑 내용 수정 요망
+        if(keywordSearchOption != false) {
+            //db 연결후 검색 시작
+        } else {
+            return -1 // false면 검색 x
+        }
+        return 0
     }
 
     //지역으로 검색
