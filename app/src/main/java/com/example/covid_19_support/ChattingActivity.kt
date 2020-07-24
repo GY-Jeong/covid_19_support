5ae502bc506246251174519fb7e0e428be667bd1
GeunYoungJeong <jyjg1124@gmail.com>
2020-07-25 00:24:48 +0900

    Chatting Activity

--- app/src/main/java/com/example/covid_19_support/ChattingActivity.kt
+++ app/src/main/java/com/example/covid_19_support/ChattingActivity.kt
@@ -1,26 +1,18 @@
 package com.example.covid_19_support
 
-import android.icu.util.UniversalTimeScale.toLong
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_chatting.*
-import java.lang.Long.parseLong
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class ChattingActivity : AppCompatActivity() {
     val messageList = ArrayList<Message>()
     lateinit var db: FirebaseFirestore
     lateinit var id: String
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting)
@@ -32,12 +24,10 @@ class ChattingActivity : AppCompatActivity() {
         connectFB()
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private fun init() {
         insertBtn.setOnClickListener {
             val current = System.currentTimeMillis()
             Log.i("time", ":$current")
-            var msg2 = Message(editText.text.toString(),editText2.text.toString(), current.toString())
             val msg = hashMapOf(
                 "제목" to editText.text.toString(),
                 "시간" to current.toString(),
@@ -54,7 +44,6 @@ class ChattingActivity : AppCompatActivity() {
         }
     }
 
-    
     private fun connectFB() {
         val doc = db.collection("chatting").document(id)
         val col = doc.collection("message")
@@ -72,11 +61,9 @@ class ChattingActivity : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 Log.e("hello", "Error getting documents.", exception)
             }
+
         messageView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
-        var adapter = MessageAdapter(messageList)
+        val adapter = MessageAdapter(messageList)
         messageView.adapter = adapter
-
-
     }
-
 }
