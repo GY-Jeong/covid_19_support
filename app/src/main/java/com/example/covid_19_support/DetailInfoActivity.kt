509105fd49cd045ed3eb1c494ccee454a44d1877
Byeong Chan Kim <dkdlxj9797@gmail.com>
2020-07-25 00:03:28 +0900

    1. DetailInfoActivity.kt 내 임시 id를 변경 2. result List에서 클릭한 내용들을 상세페이지에서 볼 수 있게 수정

--- app/src/main/java/com/example/covid_19_support/DetailInfoActivity.kt
+++ app/src/main/java/com/example/covid_19_support/DetailInfoActivity.kt
@@ -7,6 +7,8 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.detail_info.*
 
 
@@ -30,7 +32,7 @@ class DetailInfoActivity : AppCompatActivity() {
         fab_sub2.setOnClickListener {
             toggleFab()
             val i = Intent(this, ChattingActivity::class.java)
-            val id = "000000349400"
+            val id = serviceID
             i.putExtra("serviceID", id)
             startActivity(i)
         }
@@ -39,8 +41,43 @@ class DetailInfoActivity : AppCompatActivity() {
     private fun init() {
         serviceID = intent.getStringExtra("ServiceID")!!
         Log.i("ServiceID", serviceID)
+        getFB()
         fab_open = AnimationUtils.loadAnimation(this, R.anim.fab_open)
         fab_close = AnimationUtils.loadAnimation(this, R.anim.fab_close)
+    }
+
+    private fun getFB() {
+        var infoDesk:String
+        var supTarget:String
+        var supContents:String
+        var applyPeriod:String
+        var requireDoc:String
+        var infoNum:String
+        var serviceAddr:String
+
+        val db = FirebaseFirestore.getInstance()
+        db.collection("corona").whereEqualTo("ID",serviceID).get()
+            .addOnSuccessListener {
+                documents ->
+                for( item in documents) {
+                    infoDesk = item["문의처 명"].toString()
+                    supTarget = item["지원대상"].toString()
+                    supContents = item["지원내용"].toString()
+                    applyPeriod = item["신청기한"].toString()
+                    requireDoc = item["구비서류"].toString()
+                    infoNum = item["문의처 전화번호"].toString()
+                    serviceAddr = item["서비스 상세 주소"].toString()
+
+                    Log.i("getFB", infoDesk.toString())
+                    Log.i("getFB", supTarget.toString())
+                    Log.i("getFB", supContents.toString())
+                    Log.i("getFB", applyPeriod.toString())
+                    Log.i("getFB", requireDoc.toString())
+                    Log.i("getFB", infoNum.toString())
+                    Log.i("getFB", serviceAddr.toString())
+                }
+
+            }
     }
 
     private fun toggleFab() {
