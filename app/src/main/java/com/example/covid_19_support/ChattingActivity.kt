f871279069d21db6bdf4f19a6026dca6ae132d74
Byeong Chan Kim <dkdlxj9797@gmail.com>
2020-07-25 01:55:03 +0900

    채팅장 정렬 + 실시간 동기화 (UI 수정 X)

--- app/src/main/java/com/example/covid_19_support/ChattingActivity.kt
+++ app/src/main/java/com/example/covid_19_support/ChattingActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_chatting.*
 
 class ChattingActivity : AppCompatActivity() {
@@ -41,12 +42,14 @@ class ChattingActivity : AppCompatActivity() {
             }.addOnFailureListener {
                 Log.i("set","파베에 쓰는 것 실패ㅠㅠ")
             }
+            connectFB()
         }
     }
 
     private fun connectFB() {
+        messageList.clear()
         val doc = db.collection("chatting").document(id)
-        val col = doc.collection("message")
+        val col = doc.collection("message").orderBy("시간",Query.Direction.ASCENDING)
         col.get()
             .addOnSuccessListener { result ->
                 for (message in result) {
@@ -57,13 +60,14 @@ class ChattingActivity : AppCompatActivity() {
                     )
                     messageList.add(newMessage)
                 }
+                messageView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+                val adapter = MessageAdapter(messageList)
+                messageView.adapter = adapter
             }
             .addOnFailureListener { exception ->
                 Log.e("hello", "Error getting documents.", exception)
             }
 
-        messageView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
-        val adapter = MessageAdapter(messageList)
-        messageView.adapter = adapter
+
     }
 }
